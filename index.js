4bf06099a60194742cda003849428216ba19c41a
Akshath Pandey <akshath.centaurus@gmail.com>
2023-11-21 20:12:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,58 @@
+// index.js
+
 // Function to retrieve data from localStorage and populate the table
 function displayUserData() {
-    const storedUserData = JSON.parse(localStorage.getItem("userDetails")) || [];
-    const table = document.getElementById('userTable');
-    let tableContent = ``;
-    
-    // Loop through each user data and create table rows
-    storedUserData.forEach(user => {
-        tableContent += `<tr class="border-2 border-gray-200 bg-gray-400">
-            <td class="py-2 px-4 text-center">${user.fullName}</td>
-            <td class="py-2 px-4 text-center">${user.emailAddress}</td>
-            <td class="py-2 px-4 text-center">${user.passwordInput}</td>
-            <td class="py-2 px-4 text-center">${user.dateOfBirth}</td>
-            <td class="py-2 px-4 text-center">${user.acceptedTerms ? 'true' : 'false'}</td>
-        </tr>`;
-    });
-    
-    // Set the innerHTML of the table
-    table.innerHTML = `
-        <tr class="border-2 border-gray-200 bg-gray-400">
-            <th>Full Name</th>
-            <th>Email Address</th>
-            <th>Password</th>
-            <th>Date of Birth</th>
-            <th>Accepted Terms?</th>
-        </tr>
-        ${tableContent}`;
+  const storedUserData = JSON.parse(localStorage.getItem("userDetails")) || [];
+  const table = document.getElementById('userDataTable');
+  let tableContent = ``;
+  
+  // Loop through each user data and create table rows
+  storedUserData.forEach(user => {
+      tableContent += `<tr class="border-2 border-gray-200 bg-gray-400">
+          <td class="py-2 px-4 text-center">${user.fullName}</td>
+          <td class="py-2 px-4 text-center">${user.emailAddress}</td>
+          <td class="py-2 px-4 text-center">${user.passwordInput}</td>
+          <td class="py-2 px-4 text-center">${user.dateOfBirth}</td>
+          <td class="py-2 px-4 text-center">${user.acceptedTerms ? 'true' : 'false'}</td>
+      </tr>`;
+  });
+  
+  // Set the innerHTML of the table
+  table.innerHTML = `
+      <tr class="border-2 border-gray-200 bg-gray-400">
+          <th>Full Name</th>
+          <th>Email Address</th>
+          <th>Password</th>
+          <th>Date of Birth</th>
+          <th>Accepted Terms?</th>
+      </tr>
+      ${tableContent}`;
 }
 
 // Function triggered on form submission
 function handleFormSubmit(event) {
-    event.preventDefault(); // Prevent the default form submission behavior
+  event.preventDefault(); // Prevent the default form submission behavior
 
-    // Get user data from form inputs
-    const user = {
-        fullName: document.getElementById("fullName").value,
-        emailAddress: document.getElementById("emailAddress").value,
-        dateOfBirth: document.getElementById("dateOfBirth").value,
-        passwordInput: document.getElementById("passwordInput").value,
-        acceptedTerms: document.getElementById("acceptTermsCheckbox").checked,
-    };
+  // Get user data from form inputs
+  const user = {
+      fullName: document.getElementById("fullName").value,
+      emailAddress: document.getElementById("emailAddress").value,
+      dateOfBirth: document.getElementById("dateOfBirth").value,
+      passwordInput: document.getElementById("passwordInput").value,
+      acceptedTerms: document.getElementById("acceptTermsCheckbox").checked,
+  };
 
-    // Retrieve existing data from localStorage or initialize an empty array
-    let userList = JSON.parse(localStorage.getItem("userDetails")) || [];
+  // Retrieve existing data from localStorage or initialize an empty array
+  let userList = JSON.parse(localStorage.getItem("userDetails")) || [];
 
-    // Add the new user data to the array
-    userList.push(user);
+  // Add the new user data to the array
+  userList.push(user);
 
-    // Update localStorage with the modified array
-    localStorage.setItem("userDetails", JSON.stringify(userList));
+  // Update localStorage with the modified array
+  localStorage.setItem("userDetails", JSON.stringify(userList));
 
-    // Refresh the displayed table
-    displayUserData();
+  // Refresh the displayed table
+  displayUserData();
 }
 
 // Set minimum and maximum dates for the Date of Birth input
